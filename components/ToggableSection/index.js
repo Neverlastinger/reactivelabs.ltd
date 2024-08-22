ee51a91deb0d105f5ceb5bdddc80ccb3605eec37
Radoslav Popov <neverlastinger@gmail.com>
2024-08-22 14:09:34 +0100

    Fix collapse/expand using grid-layout

--- components/ToggableSection/index.js
+++ components/ToggableSection/index.js
@@ -1,35 +1,19 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
-import useResize from "hooks/useResize";
+import { useRef, useState } from "react";
 import cn from "classnames";
 import styles from "./index.module.scss";
 
 export default function ToggableSection({ head, body }) {
   const collapsibleRef = useRef();
   const [collapsed, setCollapsed] = useState(false);
-  const [height, setHeight] = useState();
-
-  useLayoutEffect(() => {
-    setHeight(collapsibleRef.current.offsetHeight);
-  }, []);
-
-  useResize(() => {
-    collapsibleRef.current.style.maxHeight = "";
-    setCollapsed(false);
-    setHeight(collapsibleRef.current.offsetHeight);
-  });
-
-  useEffect(() => {
-    if (!collapsed) {
-      collapsibleRef.current.style.maxHeight = `${height}px`;
-    } else {
-      collapsibleRef.current.style.maxHeight = "0px";
-    }
-  }, [collapsed, height]);
 
   return (
-    <div className={styles.section}>
+    <div
+      className={cn(styles.section, {
+        [styles.collapsed]: collapsed,
+      })}
+    >
       <div
         className={styles.collapseToggle}
         onClick={() => {
@@ -39,12 +23,7 @@ export default function ToggableSection({ head, body }) {
         {head}
       </div>
 
-      <ul
-        className={cn(styles.collapsible, {
-          [styles.collapsed]: collapsed,
-        })}
-        ref={collapsibleRef}
-      >
+      <ul className={styles.collapsible} ref={collapsibleRef}>
         {body}
       </ul>
     </div>
